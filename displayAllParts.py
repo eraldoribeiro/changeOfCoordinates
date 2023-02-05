8619feca023fa2e2fae4195232e306a76d062d42
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2023-02-05 14:38:23 -0500

    displayAllParts.py

---
+++ displayAllParts.py
@@ -0,0 +1,19 @@
+"""Showing all mesh parts segmented by color"""
+from vedo import *
+import os  
+
+
+# Read mesh file 
+#fileName = 'https://people.sc.fsu.edu/~jburkardt/data/ply/chopper.ply'
+
+# Read mesh files of each part, and color-label them
+mainBodyMesh = Mesh("./main_body.vtk").c("white")
+topRotorMesh = Mesh("./top_rotor.vtk").c("red")
+tailRotorMesh = Mesh("./tail_rotor.vtk").c("blue")
+
+# Show everything 
+plt = show([mainBodyMesh, topRotorMesh, tailRotorMesh], __doc__, bg='black', bg2='bb', interactive=True, axes=1, viewup='z')
+
+
+plt.close()
+
