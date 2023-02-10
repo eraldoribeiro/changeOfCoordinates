e107769be25b7b951f2c6cdde96e81db2d01d41f
Eraldo Ribeiro <eribeiro@cs.fit.edu>
2023-02-10 06:15:44 -0500

    improved and cleaned displayAll.py

--- displayAllParts.py
+++ displayAllParts.py
@@ -1,15 +1,35 @@
 """Showing all mesh parts segmented by color"""
 from vedo import *
 import os  
+from os import path
+import wget
 
 
-# Read mesh file 
-#fileName = 'https://people.sc.fsu.edu/~jburkardt/data/ply/chopper.ply'
+# Path to github raw content of files. 
+# We need to use raw.githubusercontent.com otherwise we download 
+# an html file 
+urlPath = "https://raw.githubusercontent.com/eraldoribeiro/changeOfCoordinates/main/"
+
+# Filenames (individual mesh parts)
+fn_mainBody = "main_body.vtk"
+fn_topRotor = "top_rotor.vtk"
+fn_tailRotor = "tail_rotor.vtk"
+  
+# Download the meshes from repository if we don't have them already 
+if not path.exists(fn_mainBody):
+    response = wget.download(urlPath + fn_mainBody)
+        
+if not path.exists(fn_topRotor): 
+    response = wget.download(urlPath + fn_topRotor)
+        
+if not path.exists(fn_tailRotor): 
+    response = wget.download(urlPath + fn_tailRotor)
+          
 
 # Read mesh files of each part, and color-label them
-mainBodyMesh = Mesh("./main_body.vtk").c("white")
-topRotorMesh = Mesh("./top_rotor.vtk").c("red")
-tailRotorMesh = Mesh("./tail_rotor.vtk").c("blue")
+mainBodyMesh = Mesh(fn_mainBody).c("white")
+topRotorMesh = Mesh(fn_topRotor).c("red")
+tailRotorMesh = Mesh(fn_tailRotor).c("blue")
 
 # Show everything 
 plt = show([mainBodyMesh, topRotorMesh, tailRotorMesh], __doc__, bg='black', bg2='bb', interactive=True, axes=1, viewup='z')
